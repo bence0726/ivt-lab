df0bf98e086ed3d4151de06826b43464f2cd47dd
meres <benze726@gmail.com>
2017-04-07 04:14:57 -0700

    switch_to_if

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -39,8 +39,7 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    switch (firingMode) {
-      case SINGLE:
+    if (firingMode == SINGLE) {
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -75,9 +74,8 @@ public class GT4500 implements SpaceShip {
             // if both of the stores are empty, nothing can be done, return failure
           }
         }
-        break;
 
-      case ALL:
+      }else{
         // try to fire both of the torpedos
         if (! primaryTorpedoStore.isEmpty() && !secondaryTorpedoStore.isEmpty()){
           boolean primarysuccess;
@@ -90,8 +88,6 @@ public class GT4500 implements SpaceShip {
           firingSuccess = false;
           wasPrimaryFiredLast = false;
         }
-
-        break;
     }
 
     return firingSuccess;
